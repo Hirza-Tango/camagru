42999965cec5ad5a1a5ef962a9004ae0395af73b
Dylan Slogrove <dslogrove@gmail.com>
2018-10-17 12:54:08 +0200

    Moved database connection

--- config/database.php
+++ config/database.php
@@ -2,4 +2,11 @@
 $DB_DSN = 'mysql:unix_socket=/goinfre/dslogrov/MAMP/mysql/tmp/mysql.sock;dbname=dslogrov_camagru;';
 $DB_USER = 'root';
 $DB_PASSWORD = 'toor';
+try {
+	$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+	echo "Database connection: ", $e->getMessage();
+	exit(1);
+}
 ?>
--- config/setup.php
+++ config/setup.php
@@ -1,13 +1,6 @@
 <?php
 require_once("database.php");
-global $DB_DSN, $DB_USER, $DB_PASSWORD;
-try {
-	$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
-	echo "Database connection: ", $e->getMessage();
-	exit(1);
-}
+global $db;
 if ($file = file_get_contents(dirname(__FILE__)."/setup.sql"))
 {
 	//I use a naive preg split to work around undocumented behaviour in PDO::exec()
